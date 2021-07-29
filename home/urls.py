47a754fbb65e583239b8ba066bd4d499c1b46d9a
Surya Pratap Singh Rathore <rathoresurya21@gmail.com>
2021-07-29 13:19:34 +0530

    add preloader

--- home/urls.py
+++ home/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 urlpatterns = [
-    path('', views.index),
-    path('contact', views.contact),
-    path('add', views.addBlog),
-    path('blog/<str:slug>', views.singleBlog),
-    path('edit/<str:slug>', views.editBlog),
-    path('delete/<int:id>', views.deleteBlog),
-    path('login', views.userLogin),
-    path('signup', views.userSignup),
-    path('logout', views.userLogout),
+    path('', views.index, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('add/', views.addBlog, name='add'),
+    path('blog/<str:slug>/', views.singleBlog, name='blog'),
+    path('edit/<str:slug>/', views.editBlog, name='blogedit'),
+    path('delete/<int:id>/', views.deleteBlog, name='blogdelete'),
+    path('login/', views.userLogin, name='login'),
+    path('signup/', views.userSignup, name='signup'),
+    path('logout/', views.userLogout, name='logout'),
     path('verify/<auth_token>', views.verify),
 ]
